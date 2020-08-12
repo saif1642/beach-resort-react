2c99f396e21c35ed31de24973504c4aa60b2aa69
saifdev1642 <saifdev1642@gmail.com>
2020-08-12 22:39:26 +0600

    navbar setup

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import { Rooms } from './pages/Rooms';
 import { SingleRoom } from './pages/SingleRoom';
 import { Error } from './pages/Error';
 
+import Navbar  from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/rooms" component={Rooms}></Route>
-      <Route exact path="/rooms/:slug" component={SingleRoom}></Route>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/rooms" component={Rooms}></Route>
+        <Route exact path="/rooms/:slug" component={SingleRoom}></Route>
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
