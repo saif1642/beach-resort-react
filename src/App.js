414b6615fa6debb26a1b48a173f9626d2f6c14c2
saifdev1642 <saifdev1642@gmail.com>
2020-08-12 22:06:43 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from  'react-router-dom';
+
+import { Home } from './pages/Home';
+import { Rooms } from './pages/Rooms';
+import { SingleRoom } from './pages/SingleRoom';
+import { Error } from './pages/Error';
 
 function App() {
   return (
     <div className="App">
-      <h1>Beach Resort</h1>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/rooms" component={Rooms}></Route>
+      <Route exact path="/rooms/:slug" component={SingleRoom}></Route>
     </div>
   );
 }
